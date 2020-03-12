fd50e736799a1a306ebb94ffa1c57ef09254c38b
abdounikarim <abdounikarim@gmail.com>
2020-03-12 14:18:49 +0100

    add constraints

--- src/Entity/Project.php
+++ src/Entity/Project.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ProjectRepository")
@@ -20,6 +21,7 @@ class Project
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank()
      */
     private $name;
 
@@ -35,6 +37,7 @@ class Project
 
     /**
      * @ORM\Column(type="text")
+     * @Assert\NotBlank()
      */
     private $description;
 
@@ -50,6 +53,7 @@ class Project
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Url()
      */
     private $link;
 
