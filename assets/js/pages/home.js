2eddb4a171c47a57db9649b938c777c172ab87ca
abdounikarim <abdounikarim@gmail.com>
2020-05-15 16:39:05 +0200

    add banner component

--- assets/js/pages/home.js
+++ assets/js/pages/home.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import Header from "../_inc/Header";
 import Footer from "../_inc/Footer";
 import Nav from "../_inc/Nav";
+import Banner from "../_inc/Banner";
 
 export default class Home extends Component {
     render() {
@@ -9,6 +10,7 @@ export default class Home extends Component {
             <div>
                 <Header />
                 <Nav />
+                <Banner />
                     <div>Home page</div>
                 <Footer />
             </div>
