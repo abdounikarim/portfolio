651b3469b1dc5fa1eb12243a96e3576a129fd4c5
abdounikarim <abdounikarim@gmail.com>
2020-07-11 10:43:25 +0200

    remove unused files

--- assets/js/api/experience_api.js
+++
@@ -1,9 +0,0 @@
-import { fetchJson } from './api';
-
-export function getLastThreeExperiences() {
-    return fetchJson('/api/experiences.jsonld?itemsPerPage=3&order[id]=DESC');
-}
-
-export function getExperiences() {
-    return fetchJson('/api/experiences.jsonld?order[id]=DESC');
-}
