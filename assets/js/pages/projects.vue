cc74d28a21f84926b7eaa63b61e75db45405fd37
abdounikarim <abdounikarim@gmail.com>
2020-09-14 16:30:14 +0200

    correcting projects.vue render

--- assets/js/pages/projects.vue
+++ assets/js/pages/projects.vue
@@ -1,33 +1,20 @@
 <template>
     <div>
-        <div v-for="project in projects">
-            <project-component :project="project" />
-        </div>
+        <portfolio-component :all="true" />
         <back-to-home-component />
     </div>
 </template>
 
 <script>
-import axios from 'axios';
-import ProjectComponent from '../components/project';
 import BackToHomeComponent from '../components/back-to-home';
+import PortfolioComponent from "../components/portfolio";
 
 export default {
     name: 'ProjectsPage',
     components: {
+        PortfolioComponent,
         BackToHomeComponent,
-        ProjectComponent,
     },
-    data() {
-        return {
-            projects: [],
-        }
-    },
-    async mounted() {
-        const response = await axios.get('/api/projects');
-
-        this.projects = response.data['hydra:member'];
-    }
 }
 </script>
 
